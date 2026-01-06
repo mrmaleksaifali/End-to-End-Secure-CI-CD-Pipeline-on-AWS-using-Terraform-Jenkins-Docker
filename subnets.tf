20ee9585004758aaf0b245c64b16ea7ed1e68a26
mrmaleksaifali <maleksaifali786@gmail.com>
2026-01-06 10:48:05 +0530

    Create subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
