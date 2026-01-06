acaf8caacfa9cbda4ab770445bef12c3a3c5607d
mrmaleksaifali <maleksaifali786@gmail.com>
2026-01-06 10:49:36 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,60 @@
+# Jenkins SG
+resource "aws_security_group" "jenkins_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "jenkins-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Database SG (Only Jenkins can access)
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "db-sg"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
